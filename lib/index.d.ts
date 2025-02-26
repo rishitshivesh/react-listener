00af2c53610a93b347ab6c1eefe3ca834ce0a603
Rishit Shivesh <rishitshivesh@gmail.com>
2025-02-27 00:55:24 +0530

    v1.0.4

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,17 +7,15 @@ interface ListenerOptions {
     passive?: boolean;
     targetSelector?: string;
 }
-type EventTarget = HTMLElement | Window | Document | null;
-type EventCallback = (event: Event) => void;
+export type EventCallback<E extends Event = Event> = (event: E) => void;
 /**
- * useListener - A versatile event listener hook for React.
- *
+ * `useListener` - A versatile event listener hook for React.
  * @param target - The target element, ref, or selector to attach the event(s) to.
  * @param eventTypes - The event name(s) (e.g., 'click' or ['mousedown', 'mouseup']).
  * @param callback - The event handler function.
  * @param options - Additional configurations for listener behavior.
  */
-export declare function useListener(target: (EventTarget | {
-    current: EventTarget | null | undefined;
-}) | undefined, eventTypes: string | string[], callback: EventCallback, options?: ListenerOptions): () => void;
+export declare function useListener<T extends EventTarget, E extends Event>(target: (T | {
+    current: T | null | undefined;
+}) | undefined, eventTypes: string | string[], callback: EventCallback<E>, options?: ListenerOptions): () => void;
 export {};
